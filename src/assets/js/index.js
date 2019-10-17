2a7630b746d4f85c9f3fe95c5de6ef14ffd577e4
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2019-10-17 23:21:16 +0900

    リファクタリングとか

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,6 +4,8 @@ import $ from "jquery";
 import BigNumber from "bignumber.js";
 
 $(()=>{
+    const KEY_DEFAULT_HOURS = "KEY_DEFAULT_HOURS";
+
     let timer = null;
     $("#btn-update").on("click",function(){
         const dayLength = $("#inp-daylength").val();
@@ -20,13 +22,17 @@ $(()=>{
         },100);
     });
 
-    const KEY_DEFAULT_HOURS = "KEY_DEFAULT_HOURS";
     const settings = localStorage.getItem(KEY_DEFAULT_HOURS);
     const defaultHours = (!settings) ? 24 : settings;
     $("#inp-daylength").val(defaultHours);
     $("#btn-update").click();
 });
 
+/**
+ * 入力エラーチェック
+ * @param {*} value 入力値
+ * @returns 入力エラーがあればtrue
+ */
 const hasError = (value) => {
     if(!value){
         return true;
@@ -41,11 +47,12 @@ const hasError = (value) => {
     }
 
     return false;
-}
+};
 
 /**
  * 1日の長さを算出
  * @param {*} hour 1日の長さ(時)
+ * @returns 1日の長さ(時)、1時間の長さ(分)、延長時間
  */
 const calculateDayDuration = (hour) => {
     //本来の1日の長さ(分)
@@ -66,7 +73,7 @@ const calculateDayDuration = (hour) => {
         hourDuration: hourDuration,
         extraTime: extraTime,
     }
-}
+};
 
 /**
  * 現在時刻を再計算する
@@ -76,30 +83,44 @@ const recalculate = (dayDuration) => {
     
     //現在時刻を取得(分)
     const now = new Date();
-    //const now = new Date("2019/01/01 23:59:00");
     const minutes = BigNumber(now.getHours()).times(60).plus(now.getMinutes());
-    
+    //延長時間に突入しているか判定
     const isInExtraTime = minutes.div(dayDuration.hourDuration).toNumber() > dayDuration.hour;
+    
+    //算出した1時間の長さと延長時間から、変換後の現在時刻を算出
     if(isInExtraTime){
         const convertedHours = dayDuration.hour;
         const convertedMinutes = BigNumber(minutes).minus(dayDuration.hourDuration.times(dayDuration.hour));
         const seconds = now.getSeconds();
         //画面表示を更新
-        $("#txt-hours").text(convertedHours);
-        $("#txt-minutes").text(convertedMinutes);
-        $("#txt-seconds").text(seconds);
-        $("#block-current").addClass("in-extra");
+        updateView(convertedHours,convertedMinutes,seconds,isInExtraTime);
     } else {
-        //算出した1時間の長さと延長時間から、変換後の現在時刻を算出
         const convertedHours = minutes.div(dayDuration.hourDuration).integerValue(BigNumber.ROUND_DOWN);
         const convertedMinutes = minutes.modulo(dayDuration.hourDuration);
         const seconds = now.getSeconds();
         //画面表示を更新
-        $("#txt-hours").text(convertedHours);
-        $("#txt-minutes").text(convertedMinutes);
-        $("#txt-seconds").text(seconds);
+        updateView(convertedHours,convertedMinutes,seconds,isInExtraTime);
+    }
+};
+
+/**
+ * 画面表示更新
+ * @param {*} convertedHours 時
+ * @param {*} convertedMinutes 分
+ * @param {*} seconds 秒
+ * @param {*} isInExtraTime 延長時間突入判定
+ */
+const updateView = (convertedHours,convertedMinutes,seconds,isInExtraTime) => {
+    $("#txt-hours").text(convertedHours);
+    $("#txt-minutes").text(convertedMinutes);
+    $("#txt-seconds").text(seconds);
+    setVisibility($("#txt-extra-title"),isInExtraTime);
+    if(isInExtraTime){
+        $("#block-current").addClass("in-extra");
+    }else{
         $("#block-current").removeClass("in-extra");
     }
+    
 };
 
 /**
@@ -111,5 +132,6 @@ const setVisibility = (obj,visibility) => {
     obj.css("visibility",(visibility) ? "visible" : "hidden");
 };
 
+//PWA化のため、ServiceWorkerを登録
 const swName = "/service-worker.js";
 navigator.serviceWorker.register(swName);
